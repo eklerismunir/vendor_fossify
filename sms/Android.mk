372593a044eb44c0bd70d480f1fa4c72b79f6fa0
Munir <160434174+eklerismunir@users.noreply.github.com>
2024-05-22 19:25:43 +0400

    fix cringe

--- sms/Android.mk
+++ sms/Android.mk
@@ -2,13 +2,11 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := SMS
-LOCAL_SRC_FILES := FakeStore.apk
+LOCAL_SRC_FILES := sms.apk
 LOCAL_MODULE_CLASS := APPS
 LOCAL_PRIVILEGED_MODULE := true
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
 LOCAL_CERTIFICATE := PRESIGNED
-LOCAL_OPTIONAL_USES_LIBRARIES := androidx.window.extensions androidx.window.sidecar
-LOCAL_REQUIRED_MODULES := privapp-permissions-com.android.vending.xml default-permissions-com.android.vending.xml
 LOCAL_PRODUCT_MODULE := true
 include $(BUILD_PREBUILT)
 
